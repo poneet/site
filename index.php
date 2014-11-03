3e37f923adec5ee81f1360f7cf3fa2b1fb1cb2c1
poneet <poneet@gmail.com>
2014-11-03 18:52:26 -0500

    www.poneet.com
    www.poneet.com

---
+++ index.php
@@ -0,0 +1,354 @@
+<!--
+
+
+
+    Hello fellow explorer...
+    The cake is a lie!
+    Han shot first.
+    The truth is out there.
+    *drops a tinfoil hat for you*
+           
+                |
+           \         /    
+               _/|
+             _/  \ 
+        __   \    \   __
+             / /  / 
+            /     \
+            \   /  |__
+            /\ /     /
+           /  /     / \__
+           \ /\     \   /\
+           /   \       /__\
+          /_  / \   /\/   
+            \/   |_/
+                  
+                  |
+                  |
+                  |
+                  |
+                  v
+              _- - - -_
+             /         \
+            /           \
+            \         O  O
+            (           _\
+              \      \__|
+               \   ____/
+                |    |
+               /      \
+
+
+
+--><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><!--
+
+
+
+    seo  \  /
+        - :D -
+         /  \
+
+
+
+--><meta name="description" content="Poneet...well sheeewt! Poneet Rahul: professional hobo. Furthermore, that is all." />
+<title>PONEET is currently on Earth and/or Azeroth.</title><!--
+
+
+
+    css
+
+
+
+--><style type="text/css">
+a:link   { color: #888					!important;}
+a:visited{ color: #888					!important;
+           text-decoration: none			!important;
+         }
+a:hover  { color: #0088ff				!important;
+           text-decoration: none			!important;
+         }
+body     { background-color: #2d2d2d			!important;
+           color: #fff                                  !important;
+           margin: 0px;
+         }
+#blender { border: thin dashed grey;                   	/*thin~1px (0,0)@top-left*/
+           background-color: #404040			!important;
+           float: left;
+           height: 253px;
+           position: absolute; left: 50px; top: 682px;
+           width: 9412px;
+         }/*      ||||*/
+#content { border: thin dashed grey;                   	/*thin~1px (0,0)@top-left*/
+           float: left;
+           height: 600px;
+           position: absolute; left: 50px; top: 81px;	
+           width: 9412px;
+         }/*      ||||*/
+#email   { background-color: #333;
+           position: absolute; left: 500px; top: 683px; 
+           width: 450px;
+         }
+#git     { background-color: #333;
+           position: absolute; left: 950px; top: 683px; 
+           text-align: center;
+           width: 450px;
+         }
+h1       { color: #888;
+           font-family: Courier, mono 			!important;
+           font-size: 8pt             			!important;
+           font-weight: normal        			!important; 
+         }
+#ip      { color: #666; }         
+p        { font-family: courier, mono; font-size: 8pt; }
+#spacerb { position: absolute; top: 1400px; }           	/*  margin-bottom replacement  */
+#spacerr { position: absolute; left: 9454px; }         	/*  margin-right replacement   */
+         /*                          ||||+52*/
+#spnm    { color: #ff7000; }
+#twitter { background-color: #333;
+           position: absolute; left: 50px; top: 683px;
+           width: 450px;
+         }  
+#utftxt  { background-color: #000			!important;
+           border: thin solid #000;
+           height: 28px;
+           position: absolute;
+           left: 50px; 
+           top: 51px;					
+           text-align: left;
+           width: 9412px;
+         }
+#x1      { background-color: #000; position: absolute; left:   51px; top: 82px; width: 450px; }
+#x2      { background-color: #000; position: absolute; left:  501px; top: 82px; width: 450px; }
+#x3      { background-color: #000; position: absolute; left:  951px; top: 82px; width: 450px; }
+#x4      { background-color: #000; position: absolute; left: 1401px; top: 82px; width: 450px; }
+#x5      { background-color: #000; position: absolute; left: 1851px; top: 82px; width: 450px; }
+#x6      { background-color: #000; position: absolute; left: 2301px; top: 82px; width: 896px; }
+#x7      { background-color: #000; position: absolute; left: 3197px; top: 82px; width: 450px; }
+#x8      { background-color: #000; border: 10px solid black; position: absolute; left: 3647px; top: 82px; width: 430px; }
+#x9      { background-color: #000; position: absolute; left: 4097px; top: 82px; width: 450px; }
+#x10     { background-color: #000; position: absolute; left: 4547px; top: 82px; width: 450px; }
+#x11     { background-color: #000; position: absolute; left: 4997px; top: 82px; width: 750px; }
+#x12     { background-color: #000; position: absolute; left: 5747px; top: 82px; width: 896px; }
+#x13     { background-color: #000; position: absolute; left: 6643px; top: 82px; width: 450px; }
+#x14     { background-color: #000; position: absolute; left: 7093px; top: 82px; width: 600px; }
+#x15     { background-color: #000; position: absolute; left: 7693px; top: 82px; width: 424px; }
+#x16     { background-color: #000; position: absolute; left: 8117px; top: 82px; width: 746px; }
+#x17     { background-color: #000; position: absolute; left: 8863px; top: 82px; width: 600px; }
+</style><!--
+
+
+                            
+    analytics by google     
+    |             /\      /\ 
+    |       /\   |  \  /\/  \    
+    |/\    /  |_/    \/      \/\  /|
+    |  \  |                     \/ |
+    |   \/                         \  /|
+    |__________________             \/ |__
+                                          \ 
+                                           |
+                                           v
+--><!--<script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>-->
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-25023405-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+ var s = document.getElementsByTagName('script')[0];
+ s.parentNode.insertBefore(ga, s);
+})();
+</script><!--
+
+
+
+    jQuery
+
+
+
+--><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><script>
+$(document).ready(function(){
+  $("#p1").hover(function(){  $("#x1").toggle(250);  });
+  $("#p2").hover(function(){  $("#x2").toggle(250);  });
+  $("#p3").hover(function(){  $("#x3").toggle(250);  });
+  $("#p4").hover(function(){  $("#x4").toggle(250);  });
+  $("#p5").hover(function(){  $("#x5").toggle(250);  });
+  $("#p6").hover(function(){  $("#x6").toggle(250);  });
+  $("#p7").hover(function(){  $("#x7").toggle(250);  });
+  $("#p8").hover(function(){  $("#x8").toggle(250);  });
+  $("#p9").hover(function(){  $("#x9").toggle(250);  });
+  $("#p10").hover(function(){ $("#x10").toggle(250); });
+  $("#p11").hover(function(){ $("#x11").toggle(250); });
+  $("#p12").hover(function(){ $("#x12").toggle(250); });
+  $("#p13").hover(function(){ $("#x13").toggle(250); });
+  $("#p14").hover(function(){ $("#x14").toggle(250); });
+  $("#p15").hover(function(){ $("#x15").toggle(250); });
+  $("#p16").hover(function(){ $("#x16").toggle(250); });
+  $("#p17").hover(function(){ $("#x17").toggle(250); });
+});
+</script>
+</head><body><!--
+
+
+
+    utf text
+    ____________________________________________________________________________________________________________
+    | PONEET RAHUL     blog      twitter/poneet      email@poneet.com      +1.416.277.6117                      |
+    |___________________________________________________________________________________________________________|
+
+
+
+--><div id="utftxt"><!--
+   |        --><h1><!--
+   |        -->&nbsp;&nbsp;&nbsp;&nbsp;<!--
+   |name    --><span id="spnm">PONEET RAHUL</span><!--
+   |        -->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<!--
+   |blog    --><a href="http://pwneet.blogspot.com/">blog</a><!--
+   |        -->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<!--
+   |twitter --><a href="http://www.twitter.com/poneet">twitter/poneet</a><!--
+   |        -->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<!--
+   |email   --><a href="mailto:email@poneet.com">email@poneet.com</a><!--
+   |        -->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<!--
+   |phone   -->+1.416.277.6117<!--
+   |        -->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<!--
+   |ip      --><span id="ip">Your ip address is: 64.231.57.220</span><!--
+   |        --></h1><!--
+--></div><!--
+
+
+
+    content
+    _______________________________
+    |     |     |     |     |     |
+    |     |     |     |     |     |
+    |     |     |     |     |     |
+    |_____|_____|_____|_____|_____|
+            |
+            |
+            |
+            |
+            v
+--><div id="content"><!--
+--><img id="p1"  src="images/potato.jpg"             alt=" O-REN-JI!!.. " /><!--
+--><img id="p2"  src="images/yogan.jpg"              alt=" Melting " /><!--
+--><img id="p3"  src="images/poneet.jpg"             alt=" About Poneet " /><!--
+--><img id="p4"  src="images/type.jpg"               alt=" Cut Type " /><!--
+--><img id="p5"  src="images/hika.jpg"               alt=" Light " /><!--
+--><img id="p6"  src="images/896.jpg"                alt=" 896 " /><!--
+--><img id="p7"  src="images/othello-side.jpg"       alt=" Othello Book " /><!--
+--><img id="p8"  src="images/futurist1909.jpg"       alt=" Futurist 1909 Manifesto Remixed " /><!--
+--><img id="p9"  src="images/illustration/eyes.jpg"  alt=" Being " /><!--
+--><img id="p10" src="images/illustration/ghost.jpg" alt=" Ghost... " /><!--
+--><img id="p11" src="images/illustration/frog.jpg"  alt=" Toxic!! " /><!--
+--><img id="p12" src="images/896i.jpg"               alt=" 896i " /><!--
+--><img id="p13" src="images/o.jpg"                  alt=" Fixture " /><!--
+--><img id="p14" src="images/120mm.jpg"              alt=" 120mm " /><!--
+--><img id="p15" src="images/tsa.jpg"                alt=" TSA Green Design Poster" /><!--
+--><img id="p16" src="images/a6c-b.png"              alt=" α6c-back " /><!--
+--><img id="p17" src="images/a6c.png"                alt=" α6c " /><!--
+--></div><!--
+--><div id="x1"  hidden="hidden"><p>&nbsp; O-REN-JI!!</p></div><!--
+--><div id="x2"  hidden="hidden"><p>&nbsp; yogan (japanese typesetting)</p></div><!--
+--><div id="x3"  hidden="hidden"><p>&nbsp; poneet</p></div><!--
+--><div id="x4"  hidden="hidden"><p>&nbsp; typeface</p></div><!--
+--><div id="x5"  hidden="hidden"><p>&nbsp; light</p></div><!--
+--><div id="x6"  hidden="hidden"><p>&nbsp; light: long-exposure photograph</p></div><!--
+--><div id="x7"  hidden="hidden"><p>&nbsp; othello: book cover design</p></div><!--
+--><div id="x8"  hidden="hidden"><p>futurist & hacker's manifesto <br /><br />(school project, mounted on a floppy disk drive and inside a 3.5" floppy diskette)</p></div><!--
+--><div id="x9"  hidden="hidden"><p>&nbsp; past/present/future</p></div><!--
+--><div id="x10" hidden="hidden"><p>&nbsp; ghost</p></div><!--
+--><div id="x11" hidden="hidden"><p>&nbsp; toxic frog snowboard</p></div><!--
+--><div id="x12" hidden="hidden"><p>&nbsp; light: long-exposure photograph</p></div><!--
+--><div id="x13" hidden="hidden"><p>&nbsp; the x-files</p></div><!--
+--><div id="x14" hidden="hidden"><p>&nbsp; 120mm photograph</p></div><!--
+--><div id="x15" hidden="hidden"><p>&nbsp; Toronto Society of Architects Green Design Poster</p></div><!--
+--><div id="x16" hidden="hidden"><p>&nbsp; The Mars Volta a6c album cover (back)</p></div><!--
+--><div id="x17" hidden="hidden"><p>&nbsp; The Mars Volta a6c album cover (front)</p></div><!--
+
+
+
+    [] ancient typesetting technique
+
+
+
+--><div id="spacerr"> <img src="images/50x50px-spacer.png" alt=" (spacer block) "/> </div><!--
+
+
+
+    [] ancient typesetting technique
+
+
+
+--><div id="spacerb"> <img src="images/50x50px-spacer.png" alt=" (spacer block) "/> </div><!--
+
+
+
+    twitter
+    ______________________
+    |Tweets              |
+    |____________________|
+    |                    |
+    |                    |
+    |                    |
+    |____________________|
+    |                    |
+    |                    |
+    |                    |
+    |____________________|
+    |                    |
+    |                    |
+    |                    |
+    |____________________|
+
+
+
+--><div id="twitter"><a class="twitter-timeline" data-dnt="true" href="https://twitter.com/poneet" data-widget-id="526950516937338880">Tweets by @poneet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div><!--
+
+
+
+    php email
+    ______________________
+    |____________________|
+    ______________________
+    |____________________|
+    ______________________
+    |                    |
+    |                    |
+    |                    |
+    |____________________|
+    _______
+    |_____|
+
+
+
+--><div id="email"><!--
+--><form action="form_script.php" method="POST"><!--
+--><pre>  Send an email via php!<br /><br /></pre><!--
+--><pre>       To: <input type="text" name="email" placeholder="email@domain.com" size="50" /></pre><!--
+--><pre>  Subject: <input type="text" name="subject" placeholder="Subject" size="50" /></pre><!--	
+--><pre>  Message: <textarea rows="10" cols="51" name="message" placeholder="Message"></textarea></pre><!--
+--><pre>           <input type="submit" name="submit" value="Send email" /></pre><!--
+--></form></div><!--
+
+
+
+    git
+
+
+
+--><div id="git"><br /><a href="https://try.github.io/levels/1/challenges/1"><img src="https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/121/completed-try-git-b54d1cd9fa940e4a3c1925056d38eca2.png" width="200" height="200"/></a><br /><br/></div><!--
+--></body></html><!--
+
+
+
+
+    ...
+
+
+
+
+-->
